32259589b5bbcfef59611b4f8e9eed22e2dd3eff
chaikovskyit <chaikovskyit@gmail.com>
2023-01-03 22:28:21 +0200

    create first basic screen

---
+++ components/CategoryGridTile.js
@@ -0,0 +1,17 @@
+import { Pressable, View, Text } from 'react-native'
+
+const CategoryGridTile = ({title, color}) => {
+  return (
+    <View>
+      <Pressable>
+        <View>
+          <Text>
+            {title}
+          </Text>
+        </View>
+      </Pressable>
+    </View>
+  )
+}
+
+export default CategoryGridTile
