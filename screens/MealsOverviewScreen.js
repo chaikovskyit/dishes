cb825fee54a283e8fd256ba61a2a5088674659c7
chaikovskyit <chaikovskyit@gmail.com>
2023-01-04 10:57:38 +0200

    added styles for MealsOverScreen, and some dummy data

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,19 +1,43 @@
-import { View, StyleSheet, Text } from 'react-native'
-import { MEALS } from '../data/dummy-data'
+import { View, StyleSheet, Text, FlatList } from "react-native";
+import { useRoute } from "@react-navigation/native";
+import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
-const MealsOverviewScreen = () => {
+const MealsOverviewScreen = ({ route }) => {
+  const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((meal) => {
+    return meal.categoryIds.indexOf(catId) >= 0;
+  });
+
+  const renderMealItem = (itemData) => {
+    const item = itemData.item;
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      duration: item.duration,
+      affordability: item.affordability,
+      complexity: item.complexity,
+    };
+    return <MealItem {...mealItemProps} />;
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default MealsOverviewScreen
+export default MealsOverviewScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16
-  }
-})
+    padding: 16,
+  },
+});
