b13367829b013cf85f41d21ed78d5d739ad3f640
chaikovskyit <chaikovskyit@gmail.com>
2023-01-05 10:46:52 +0200

    added text for empty favorite screen

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,18 +1,42 @@
-import { useContext } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import MealsList from '../components/MealsList/MealsList'
-import { FavoritesContext} from '../store/context/favorite-context'
-import {MEALS} from '../data/dummy-data'
+import { useContext } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import MealsList from "../components/MealsList/MealsList";
+import { FavoritesContext } from "../store/context/favorite-context";
+import { MEALS } from "../data/dummy-data";
 
 const FavoritesScreen = () => {
+  const FavoriteMealsCtx = useContext(FavoritesContext);
 
-  const FavoriteMealsCtx = useContext(FavoritesContext)
+  const favoriteMeals = MEALS.filter((meal) =>
+    FavoriteMealsCtx.ids.includes(meal.id)
+  );
 
-  const favoriteMeals = MEALS.filter(meal => FavoriteMealsCtx.ids.includes(meal.id) )
+  if (favoriteMeals.length === 0) {
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>You have no favorite meals yet.</Text>
+      </View>
+    );
+  }
 
   return (
-    <MealsList items={favoriteMeals}/>
-  )
-}
 
-export default FavoritesScreen
+      <MealsList items={favoriteMeals} />
+
+  );
+};
+
+export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white'
+  }
+});
